d760ff017ca7006f1a6814327ab186dcf58161ff
khalaidw <walidkhalaid@outlook.com>
2026-01-09 22:49:32 +0000

    part 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
+  // centralized dbs
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
+
+  try {
+    const currentDB = await central(id);
+
+    let basicData = await dbs[currentDB](id);
+
+    let vaultData = await vault(id);
+
+    console.log({ id, ...basicData, ...vaultData });
+  } catch (err) {
+    console.error(`X Error: ${err.message}`);
+  }
 }
+
+console.log(getUserData(5));
